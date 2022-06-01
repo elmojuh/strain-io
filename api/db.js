5e8feee3618d1f27a350b41131f2182d958d8a5e
rafael <raf4ael@gmail.com>
2022-06-01 16:16:39 -0300

    subindo readme e acertando uso de variável .env

--- api/db.js
+++ api/db.js
@@ -9,7 +9,7 @@
 
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/strainIO");
+mongoose.connect("MONGODB_URL");
 var conn = mongoose.connection;
 conn.on('connected', function() {
     console.log('database is connected successfully');
@@ -18,7 +18,7 @@ conn.on('disconnected',function(){
     console.log('database is disconnected successfully');
 })
 conn.on('error', console.error.bind(console, 'connection error:'));
-module.exports = conn;
 
 module.exports = conn;
+
 
